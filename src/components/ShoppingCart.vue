d62d717162acd4cd6ac03b6b421ab5f4ff201ba8
Andre Madarang <andre_madarang@hotmail.com>
2022-11-05 14:03:10 -0400

    Starting point

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,224 @@
+<script setup>
+import { computed, ref } from 'vue'
+
+const items = ref([
+  {
+    id: 1,
+    name: 'MacBook Pro',
+    description: 'Description for MacBook Pro.',
+    image: new URL('../assets/macbookpro.jpg', import.meta.url).href,
+    price: 349999,
+    quantity: 1,
+  },
+  {
+    id: 2,
+    name: 'Mac Studio',
+    description: 'Description for Mac Studio.',
+    image: new URL('../assets/macstudio.jpg', import.meta.url).href,
+    price: 399999,
+    quantity: 1,
+  },
+  {
+    id: 3,
+    name: 'iPhone Pro',
+    description: 'Description for iPhone Pro.',
+    image: new URL('../assets/iphonepro.jpg', import.meta.url).href,
+    price: 99999,
+    quantity: 1,
+  },
+  {
+    id: 4,
+    name: 'iPad Pro',
+    description: 'Description for iPad Pro.',
+    image: new URL('../assets/ipadpro.jpg', import.meta.url).href,
+    price: 129999,
+    quantity: 1,
+  },
+])
+
+function updateQuantity(event, item) {
+  item.quantity = parseInt(event.target.value)
+}
+
+function deleteItem(id) {
+  items.value = items.value.filter(item => item.id !== id)
+}
+
+const numberOfItems = computed(() => {
+  return items.value.reduce((prev, curr) => prev + curr.quantity, 0)
+})
+
+const subtotal = computed(() => {
+  return items.value.reduce(
+    (prev, curr) => prev + curr.quantity * curr.price,
+    0
+  )
+})
+
+const tax = computed(() => {
+  return Math.round(subtotal.value * 0.13)
+})
+
+const total = computed(() => {
+  return subtotal.value + tax.value
+})
+</script>
+
+<template>
+  <div v-if="items.length > 0" class="shopping-cart-container">
+    <div class="cart-items-container">
+      <div v-for="item in items" :key="item.id" class="cart-item">
+        <div class="cart-item-left">
+          <div class="cart-item-image">
+            <img
+              :src="item.image"
+              class="cart-item-image"
+              alt="item
+                image"
+            />
+          </div>
+          <div class="cart-item-details">
+            <div class="cart-item-name">{{ item.name }}</div>
+            <div class="cart-item-description">{{ item.description }}</div>
+          </div>
+        </div>
+        <div class="cart-item-right">
+          <div class="cart-item-quantity">
+            <input
+              @change="updateQuantity($event, item)"
+              type="number"
+              value="1"
+              min="1"
+              max="10"
+            />
+          </div>
+          <div class="cart-item-total">${{ item.price / 100 }}</div>
+          <div class="cart-item-remove">
+            <button @click="deleteItem(item.id)">&times;</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="cart-totals-container">
+      <div class="cart-total-labels">
+        <div># of Items</div>
+        <div>Subtotal</div>
+        <div>Tax (13%)</div>
+        <div class="cart-total">Total</div>
+      </div>
+      <div class="cart-total-values">
+        <div>{{ numberOfItems }}</div>
+        <div>${{ subtotal / 100 }}</div>
+        <div>${{ tax / 100 }}</div>
+        <div class="cart-total">${{ total / 100 }}</div>
+      </div>
+    </div>
+  </div>
+
+  <div v-else class="shopping-cart-container">
+    There are no items in your cart.
+  </div>
+</template>
+
+<style scoped>
+.shopping-cart-container {
+  margin: 20px 0;
+}
+
+.cart-item {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  border-top: 1px solid lightgray;
+  padding: 10px 0;
+}
+
+.cart-item:last-child {
+  border-bottom: 1px solid lightgray;
+}
+
+.cart-item-left {
+  display: flex;
+}
+
+.cart-item-right {
+  display: flex;
+  align-items: center;
+  padding: 12px 0;
+}
+
+.cart-item-image {
+  display: none;
+  width: 70px;
+  height: 70px;
+}
+
+.cart-item-details {
+  padding: 0 10px;
+}
+
+.cart-item-description {
+  margin-top: 10px;
+  color: gray;
+  line-height: 1.2;
+  font-size: 14px;
+}
+
+.cart-item-quantity,
+.cart-item-total {
+  width: 20px;
+  padding: 0 22px;
+}
+
+.cart-item-remove {
+  padding-left: 40px;
+}
+
+.cart-item-remove > button {
+  font-size: 18px;
+}
+
+.cart-totals-container {
+  display: grid;
+  justify-content: end;
+  grid-template-columns: 120px 120px;
+  text-align: right;
+  line-height: 2.2;
+  margin: 20px 0;
+  padding: 10px;
+}
+
+.cart-total-labels {
+  text-transform: uppercase;
+  color: gray;
+}
+
+.cart-total {
+  font-weight: bold;
+}
+
+@media (min-width: 500px) {
+  .cart-item {
+    padding: 20px 0;
+  }
+  .cart-item-image {
+    display: block;
+  }
+
+  .cart-item-quantity,
+  .cart-item-total {
+    width: 36px;
+    padding: 0 30px;
+  }
+
+  .cart-item {
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 20px 0;
+  }
+
+  .cart-item-right {
+    padding: 0;
+  }
+}
+</style>
---
+++ src/components/WeatherWidget.vue
@@ -0,0 +1,158 @@
+<script setup></script>
+
+<template>
+  <div class="weather-widget">
+    <div class="current-weather-container">
+      <div class="location-container">
+        <div class="location-city">Toronto</div>
+        <div class="location-country">Canada</div>
+      </div>
+      <div>
+        <img
+          src="../assets/sunny.svg"
+          alt="sunny"
+          class="current-weather-icon"
+        />
+      </div>
+      <div>
+        <div class="current-temperature">23 &#8451;</div>
+        <div class="current-description">Partly Cloudy</div>
+      </div>
+    </div>
+
+    <div class="weekly-weather-container">
+      <div class="day-container">
+        <div class="day">Sat</div>
+        <div class="day-icon">
+          <img src="../assets/sunny.svg" class="day-icon" alt="weather icon" />
+        </div>
+        <div class="temperature-container">
+          <div>23 &#8451;</div>
+          <div>12 &#8451;</div>
+        </div>
+      </div>
+      <div class="day-container">
+        <div class="day">Sun</div>
+        <div class="day-icon">
+          <img src="../assets/cloudy.svg" class="day-icon" alt="weather icon" />
+        </div>
+        <div class="temperature-container">
+          <div>23 &#8451;</div>
+          <div>12 &#8451;</div>
+        </div>
+      </div>
+      <div class="day-container">
+        <div class="day">Mon</div>
+        <div class="day-icon">
+          <img src="../assets/rain.svg" class="day-icon" alt="weather icon" />
+        </div>
+        <div class="temperature-container">
+          <div>23 &#8451;</div>
+          <div>12 &#8451;</div>
+        </div>
+      </div>
+      <div class="day-container">
+        <div class="day">Tue</div>
+        <div class="day-icon">
+          <img
+            src="../assets/partly-cloudy.svg"
+            class="day-icon"
+            alt="weather icon"
+          />
+        </div>
+        <div class="temperature-container">
+          <div>23 &#8451;</div>
+          <div>12 &#8451;</div>
+        </div>
+      </div>
+      <div class="day-container">
+        <div class="day">Wed</div>
+        <div class="day-icon">
+          <img src="../assets/sunny.svg" class="day-icon" alt="weather icon" />
+        </div>
+        <div class="temperature-container">
+          <div>23 &#8451;</div>
+          <div>12 &#8451;</div>
+        </div>
+      </div>
+      <div class="day-container">
+        <div class="day">Thu</div>
+        <div class="day-icon">
+          <img src="../assets/sunny.svg" class="day-icon" alt="weather icon" />
+        </div>
+        <div class="temperature-container">
+          <div>23 &#8451;</div>
+          <div>12 &#8451;</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.weather-widget {
+  font-family: sans-serif;
+  background: #1d567c;
+  color: white;
+  border-radius: 8px;
+  line-height: 1.4;
+}
+
+.current-weather-container {
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  padding: 14px 10px;
+  text-align: center;
+}
+
+.location-container {
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+.current-temperature {
+  font-size: 28px;
+}
+
+.current-weather-icon {
+  width: 64px;
+}
+
+.weekly-weather-container {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  justify-content: space-around;
+  text-align: center;
+  margin: 10px 0;
+}
+
+.day-container {
+  font-size: 14px;
+  padding: 14px 10px;
+}
+
+.day {
+  text-transform: uppercase;
+}
+
+.day-icon {
+  width: 28px;
+  padding: 2px 0;
+  margin: auto;
+}
+
+@media (min-width: 400px) {
+  .current-weather-container {
+    flex-direction: row;
+    gap: 0;
+  }
+
+  .weekly-weather-container {
+    flex-direction: row;
+  }
+}
+</style>
